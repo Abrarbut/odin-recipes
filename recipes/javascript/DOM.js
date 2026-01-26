c0506b9fe4e19ad16a54274d529cafcf937b634f
abrarbuttmi7@gmail.com <abrarbuttmi7@gmail.com>
2026-01-26 17:23:17 +0500

    method 1 of event added

--- recipes/javascript/DOM.js
+++ recipes/javascript/DOM.js
@@ -1,20 +1,24 @@
-// your JavaScript file
-const container = document.querySelector("#container");
-const redpara = document.createElement("p");
-redpara.textContent = "Hey I’m red!";
-redpara.style.color = "red";container.appendChild(redpara);const blueh3 = document.createElement("h3");
-blueh3.textContent = "I’m a blue h3!";
-blueh3.style.color = "blue";
-container.appendChild(blueh3);
-container.appendChild(redpara);
+// // your JavaScript file
+// const container = document.querySelector("#container");
+// const redpara = document.createElement("p");
+// redpara.textContent = "Hey I’m red!";
+// redpara.style.color = "red";container.appendChild(redpara);const blueh3 = document.createElement("h3");
+// blueh3.textContent = "I’m a blue h3!";
+// blueh3.style.color = "blue";
+// container.appendChild(blueh3);
+// container.appendChild(redpara);
 
-const divdetail = document.createElement("div");
-const divh1 = document.createElement("h1");
-divh1.textContent = "I’m in a div";
-const divp = document.createElement("p");
-divp.textContent = "ME TOO!";
-divdetail.appendChild(divh1);
-divdetail.appendChild(divp);
-divdetail.style.border = "2px solid black";
-divdetail.style.backgroundColor = "pink";
-container.appendChild(divdetail);
+// const divdetail = document.createElement("div");
+// const divh1 = document.createElement("h1");
+// divh1.textContent = "I’m in a div";
+// const divp = document.createElement("p");
+// divp.textContent = "ME TOO!";
+// divdetail.appendChild(divh1);
+// divdetail.appendChild(divp);
+// divdetail.style.border = "2px solid black";
+// divdetail.style.backgroundColor = "pink";
+// container.appendChild(divdetail);
+
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
