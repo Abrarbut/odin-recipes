9c84c1201eaada25fd839b812df8700072041bee
abrarbuttmi7@gmail.com <abrarbuttmi7@gmail.com>
2026-01-26 16:57:41 +0500

    DOM exercise Done

--- recipes/javascript/DOM.js
+++ recipes/javascript/DOM.js
@@ -1,8 +1,20 @@
 // your JavaScript file
 const container = document.querySelector("#container");
+const redpara = document.createElement("p");
+redpara.textContent = "Hey I’m red!";
+redpara.style.color = "red";container.appendChild(redpara);const blueh3 = document.createElement("h3");
+blueh3.textContent = "I’m a blue h3!";
+blueh3.style.color = "blue";
+container.appendChild(blueh3);
+container.appendChild(redpara);
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
-
-container.appendChild(content);
+const divdetail = document.createElement("div");
+const divh1 = document.createElement("h1");
+divh1.textContent = "I’m in a div";
+const divp = document.createElement("p");
+divp.textContent = "ME TOO!";
+divdetail.appendChild(divh1);
+divdetail.appendChild(divp);
+divdetail.style.border = "2px solid black";
+divdetail.style.backgroundColor = "pink";
+container.appendChild(divdetail);
